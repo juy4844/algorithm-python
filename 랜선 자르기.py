82a869643037cacde3ae975967f40de3d4270456
juy4844 <uiyeop@ajou.ac.kr>
2025-01-03 14:46:14 +0900

    랜선 자르기

---
+++ 랜선 자르기.py
@@ -0,0 +1,23 @@
+k, n = map(int, input().split())
+
+arr = []
+
+for i in range(k):
+    arr.append(int(input()))
+
+maxv = max(arr)
+minv = 1
+
+while minv < maxv:
+    mid = (maxv + minv) // 2
+    cnt = 0
+
+    for i in range(k):
+        cnt += (arr[i] // mid)
+
+    if cnt >= n:
+        minv = mid + 1
+    else:
+        maxv = mid
+    
+print(maxv)
